4e0004300cd42eedc524130eb2b8b63362a3a2f1
gribu4ok <162813176+gribu4ok@users.noreply.github.com>
2024-04-13 13:35:41 +0200

    Laba3 add

---
+++ src/main/java/com/mycompany/laba3/Laba3.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.laba3;
+
+/**
+ *
+ * @author gribu
+ */
+public class Laba3 {
+
+    public static void main(String[] args) {
+        MyWindow window = new MyWindow();
+        window.setVisible(true);
+    }
+}
